b815c26ae244d0e57a31f2c7e4f118acc84fa479
thanhthao0212 <120547547+thanhthao0212@users.noreply.github.com>
2023-06-28 21:41:02 +0700

    a

---
+++ ShradhaGeneralBookStores/src/app/components/publisher/publisher.component.ts
@@ -0,0 +1,16 @@
+import { Component,OnInit } from "@angular/core";
+
+
+@Component({
+    templateUrl: './publisher.component.html',
+
+})
+
+export class publisherComponent implements OnInit{
+
+    ngOnInit() {
+
+
+    }
+
+}
